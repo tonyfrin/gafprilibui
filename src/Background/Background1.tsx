564ffaf75cac2c9bab38537362a3282da825771c
Anthony Frassino <tonyfrin@gmail.com>
2023-03-15 18:05:06 -0500

    Version 0.1.1 Background1 y @Media de LoginHeader

---
+++ src/Background/Background1.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { css } from '@emotion/css';
+
+type style = {
+  image: string;
+  children: JSX.Element;
+};
+
+const Background1Styles = (image: string) => css`
+    background-image: url(${image});
+    background-size: 185vh;
+    background-repeat: no-repeat;
+    min-height: 100vh;
+`;
+
+export const Background1 =  ({ children, image }: style) => {
+  return (
+    <div className={Background1Styles(image)}>
+      {children}
+    </div>
+  )
+}

0e49a94547f1fadd6f0acd5b8e38de12e6e37616
Anthony Frassino <tonyfrin@gmail.com>
2023-04-21 15:32:14 -0500

    Ver 0.2.25 Update Button margin

---
+++ dist/Constans/DocumentsIndex 3.js
@@ -0,0 +1,26 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.DocumentsIndex = void 0;
+var DocumentsIndex = {
+  VE: [{
+    label: 'J',
+    value: 'J'
+  }, {
+    label: 'G',
+    value: 'G'
+  }, {
+    label: 'V',
+    value: 'V'
+  }, {
+    label: 'No aplica',
+    value: 'null'
+  }],
+  US: [{
+    label: 'No aplica',
+    value: 'null'
+  }]
+};
+exports.DocumentsIndex = DocumentsIndex;
